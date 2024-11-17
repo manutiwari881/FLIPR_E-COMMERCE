5f408d556ad4170bcbc3c737211085fbd37e21e7
manutiwari881 <164267790+manutiwari881@users.noreply.github.com>
2024-11-17 15:36:50 +0530

    Add files via upload
    FLIPR_PROJECT

---
+++ src/main/java/Backend/E_commerce/ECommerceApplication.java
@@ -0,0 +1,12 @@
+package Backend.E_commerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceApplication.class, args);
+	}
+}
